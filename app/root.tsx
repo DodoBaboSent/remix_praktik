2bd2881b01a65ab11051053511960751c073416f
Наугольнов Денис Игоревич <Naugolnov@dvagp.ru>
2024-03-05 16:34:07 +1000

    Coverting previous site

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
+
 import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
@@ -8,25 +8,114 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { useEffect, useState } from "react";
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  {
+    rel: "icon",
+    href: "/favicon.ico",
+    type: "image/png",
+  },
+  {
+    rel: "stylesheet",
+    href: "/style.css",
+  },
+  {
+    rel: "stylesheet",
+    href: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
+    integrity: "sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC",
+    crossOrigin: "anonymous"
+  },
 ];
 
 export default function App() {
+
+  const [windowSize, setWindowSize] = useState({
+    windowWidth: 0
+  })
+
+  useEffect(() => {
+    const currentWindowWidth = window.innerWidth;
+    setWindowSize({
+      windowWidth: currentWindowWidth,
+    });
+  }, []);
+
+
+  useEffect(() => {
+    const handleWindowSize = () => {
+      const currentWindowWidth = window.innerWidth;
+      setWindowSize({ windowWidth: currentWindowWidth })
+    };
+    window.addEventListener('resize', handleWindowSize)
+    return () => {
+      window.removeEventListener('resize', handleWindowSize)
+    };
+  }, [windowSize.windowWidth]);
+
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=1366, initial-scale=1" />
         <Meta />
         <Links />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></script>
       </head>
-      <body>
-        <Outlet />
+      <body className="d-flex flex-column">
         <ScrollRestoration />
-        <Scripts />
         <LiveReload />
+        <header className="bg-header-right d-flex flex-row w-100" style={{ justifyContent: (windowSize.windowWidth >= 1366) ? "space-around" : undefined }}>
+          <div className="d-flex align-items-center logo">
+            <img src="assets/logoDVAGP.gif" alt="logo" className="img-logo" />
+          </div>
+          <div className="d-flex flex-column justify-content-center">
+            <div className="d-flex flex-row mb-3">
+              <div className="bar"></div>
+              <p className="padded text-white m-0 fw-bold">Акционерное общество</p>
+            </div>
+            <h1 className="text-bold name">Дальневосточное аэрогеодезическое предприятие</h1>
+          </div>
+          <div className="d-flex flex-column align-items-center justify-content-center">
+            <div className="img-inside">
+              <input type="text" className="search-box ps-4_5"></input>
+            </div>
+          </div>
+        </header>
+        <div className="container-fluid flex-row mt-4 mb-5">
+          <div className="row px-2">
+            <Outlet />
+            <aside className="d-flex flex-column col col-lg-3 col-xl-3 col-xxl-3">
+              <h1 className="mb-4 text-start text-blue mb-4">Новости</h1>
+              <div className="d-flex flex-column">
+                <p className="text-start fs-6 gray-text mb-0 mt-0">11.08.2022</p>
+                <p className="text-start"><a href="http://dvagp.ru/news/39436/" className="text-inherit fw-bold">Сообщение о проведении внеочередного Общего собрания акционеров акционерного общества «Дальневосточное аэрогеодезическое предприятие»</a></p>
+              </div>
+              <div className="d-flex flex-row align-items-center mt-3">
+                <img src="assets/icon-archive.gif" alt="archive" className="me-1" />
+                <p className="mb-0"><a href="news/index.html" className="text-inherit">Архив новостей</a></p>
+              </div>
+            </aside>
+          </div>
+        </div>
+        <footer className="container-fluid flex-row pt-4">
+          <div className="row pb-4">
+            <aside className="col col-lg-3 col-xl-2 col-xxl-2 d-flex flex-column">
+              <p>
+                <strong>© АО «ДВ АГП», 2023</strong> <br />
+                Все права защищены и принадлежат <br />
+                компании АО «ДВ АГП»
+              </p>
+            </aside>
+            <main className="col col-lg-6 col-xl-7 col-xxl-7 d-flex flex-column">
+              <p><strong>Контактные данные</strong><br />680000, Россия, г.Хабаровск, ул. Шеронова, 97<br />Тел.: (4212) 30-43-19, 32-40-58 Факс: 30-43-19<br />E-mail: dvagp@dvagp.ru</p>
+            </main>
+            <aside className="col col-lg-3 col-xl-2 col-xxl-2 d-flex flex-column">
+              <p>Разработка сайта, 2023<br />Студент ХКОТСО Наугольнов А.И.</p>
+            </aside>
+          </div>
+        </footer>
+        <Scripts />
       </body>
     </html>
   );
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,44 @@
 import type { MetaFunction } from "@remix-run/node";
+import { NavComponent } from "./nav"
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Главная | Дальневосточное АГП" },
+    { name: "description", content: "Главная страница предприятия" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <aside className="d-flex flex-column col col-lg-3 col-xl-2 col-xxl-2">
+        <div className="d-flex flex-row padded-menu align-items-center">
+          <p className="to-home nav-elem-1">
+            <a href="#" className="dotted-underline">На главную</a>
+          </p>
+          <div className="nav-elem-2">
+            <a href="mailto:dvagp@dvagp.ru" className="text-decoration-none">
+              <img src="assets/icon-contacts.gif" alt="contacts" />
+            </a>
+          </div>
+          <div>
+            <a href="sitemap/index.html" className="text-decoration-none" />
+            <a href="sitemap/index.html" className="text-decoration-none">
+              <img src="assets/icon-sitemap.gif" alt="sitemap" />
+            </a>
+          </div>
+        </div>
+        <div className="d-flex flex-column mt-4">
+          <div className="padded-map">
+            <p className="text-white text-wrap mb-0 text-center"><a href="#" className="text-inherit">Карта Дальнего Востока (топографическая)</a></p>
+          </div>
+        </div>
+      </aside>
+      <main className="d-flex flex-column col col-lg-6 col-xl-7 col-xxl-7">
+        <h1 className="mb-4 text-start text-blue">О нашем предприятии</h1>
+        <p className="text-start mt-0 smaller-text">Предприятие осуществляет топографо-геодезическое и картографическое обеспечение территории Дальнего Востока Российской Федерации, включая Хабаровский и Камчатский края,  Амурскую и Сахалинскую области, Еврейскую автономную область, что составляет около 2 миллионов квадратных километров, без учета территории шельфа дальневосточных морей.</p>
+        <p className="padded-annot smaller-text">Имеет структурные подразделения в городах Хабаровске, Южно-Сахалинске, Петропавловске-Камчатском, Свободном</p>
+      </main>
+    </>
   );
 }
